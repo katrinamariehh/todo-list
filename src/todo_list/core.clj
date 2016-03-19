583c0cc9470245cbaddd2f1109459a5ccbceee64
Katrina Hall <katrina@Katrinas-MBP.home>
2016-03-18 22:34:25 -0400

    Added ring HTTP library, serving up a / endpoint with a construction gif :)

--- src/todo_list/core.clj
+++ src/todo_list/core.clj
@@ -5,3 +5,20 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn home [request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "<h1>Super cool awesome headline</h1>
+                     <img src='http://www.cs.utah.edu/~gk/atwork/img/hevequip2.gif'
+                     alt='who doesn't love a good construction gif'>"})
+
+(defn error [request]
+  {:status 404
+   :headers {"Content-Type" "text/html"}
+   :body "<h1>404 Not Found</h1"})
+
+(defn handler [request]
+  (case (request :uri)
+    "/" (home request)
+    (error request)))
